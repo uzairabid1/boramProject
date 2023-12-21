84d55c50cf53d870701975b5469ef05107e150a2
Uzair Abid <89099804+uzairabid1@users.noreply.github.com>
2023-12-21 23:17:18 +0500

    Update naver.py

--- naver.py
+++ naver.py
@@ -44,6 +44,7 @@ from itertools import product
 options = webdriver.ChromeOptions() 
 options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--headless=new')
+options.add_argument('--no-sandbox')
 
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()),options=options)
