7fe4e4e6d35fd9d77d71cc5701aad9b72dbe5037
Uzair Abid <89099804+uzairabid1@users.noreply.github.com>
2023-12-21 22:25:47 +0500

    Update naver.py

--- naver.py
+++ naver.py
@@ -43,11 +43,9 @@ from itertools import product
 # driver = webdriver.Chrome(service=Service) # --> run this code once then deactiuvate it like shift + #
 options = Options()
 options.add_argument("--headless=new")
-options.add_argument('--no-sandbox') # Bypass OS security model
-options.add_argument('--disable-gpu')  # applicable to windows os only
-options.add_argument('start-maximized') # 
-options.add_argument('disable-infobars')
 options.add_argument("--disable-extensions")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(options=options)
 
 
