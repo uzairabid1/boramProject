79ab1d987395d235839d1d459ddef0c162c8e9b6
Uzair Abid <89099804+uzairabid1@users.noreply.github.com>
2024-02-02 15:19:12 +0500

    changes

--- csvToExcel.py
+++ csvToExcel.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-data = pd.read_csv('naver_copy_sample.csv')
-writer = pd.ExcelWriter('output_ulta.xlsx')
+data = pd.read_csv('./output/naver_copy_sample.csv', encoding='ISO-8859-1')
+writer = pd.ExcelWriter('./output/output_ulta.xlsx')
 data.to_excel(writer, index=False,encoding='utf-8')
 writer.save()
